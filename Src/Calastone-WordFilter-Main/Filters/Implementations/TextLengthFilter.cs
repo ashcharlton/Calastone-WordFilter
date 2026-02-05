1694daa7a2a63b343acfff6596beea1787e3b7e5
Ashley Charlton <ashleycharlton@outlook.com>
2026-02-05 11:05:22 +0000

    Added interface for the TextFilter. Added the TextLengthFilter implementation

---
+++ Src/Calastone-WordFilter-Main/Filters/Implementations/TextLengthFilter.cs
@@ -0,0 +1,24 @@
+﻿using Calastone_WordFilter_Main.Filters.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calastone_WordFilter_Main.Filters.Implementations
+{
+    public class TextLengthFilter : ITextFilter
+    {
+        private readonly int _minTextLength;
+
+        public TextLengthFilter(int minTextLength)
+        {
+            _minTextLength = minTextLength;
+        }
+
+        public bool IsValidText(string text)
+        {
+            return text.Length >= _minTextLength;
+        }
+    }
+}
---
+++ Src/Calastone-WordFilter-Main/Filters/Interfaces/ITextFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calastone_WordFilter_Main.Filters.Interfaces
+{
+    public interface ITextFilter
+    {
+        bool IsValidText(string text);
+    }
+}
