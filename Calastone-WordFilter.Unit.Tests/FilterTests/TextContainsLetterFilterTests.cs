bf6867359bf2a68e7b2f547f1b7a1bd60642965d
Ashley Charlton <ashleycharlton@outlook.com>
2026-02-05 11:31:10 +0000

    Added the TextContainsLetterFilter. Added unit tests for filter

---
+++ Calastone-WordFilter.Unit.Tests/FilterTests/TextContainsLetterFilterTests.cs
@@ -0,0 +1,39 @@
+﻿using Calastone_WordFilter_Main.Filters.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calastone_WordFilter.Unit.Tests.FilterTests
+{
+    public class TextContainsLetterFilterTests
+    {
+        [Theory]
+        [InlineData("Fact", 't', true, false)]
+        [InlineData("Fact", 'T', false, true)]
+        [InlineData("Fact", 'T', true, false)]
+        [InlineData("FacT", 'T', true, false)]
+        [InlineData("Face", 't', true, true)]
+        [InlineData("Face", 't', false, true)]
+        [InlineData("", 't', false, false)]
+        [InlineData("", 't', true, false)]
+        [InlineData("", 'T', false, false)]
+        [InlineData("", 'T', true, false)]
+        [InlineData(null, 'T', true, false)]
+        [InlineData(null, 'T', false, false)]
+        [InlineData(null, 't', true, false)]
+        [InlineData(null, 't', false, false)]
+        public void Checking_Text_Contains_Letter_Return_Correct_Valid_Response(string text, char containingLetter, bool ignoreCase, bool isTextValid)
+        {
+            // Arrange
+            var textContainsLetterFilter = new TextContainsLetterFilter(containingLetter, ignoreCase);
+
+            // Act
+            var isValid = textContainsLetterFilter.IsValidText(text);
+
+            // Assert
+            Assert.Equal(isTextValid, isValid);
+        }
+    }
+}
---
+++ Src/Calastone-WordFilter-Main/Filters/Implementations/TextContainsLetterFilter.cs
@@ -0,0 +1,37 @@
+﻿using Calastone_WordFilter_Main.Filters.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calastone_WordFilter_Main.Filters.Implementations
+{
+    public class TextContainsLetterFilter : ITextFilter
+    {
+        // Using a char to force a single letter. No need to check a string for a single letter.
+        // Making the code more readable. 
+        private readonly char _containingLetter;
+        private readonly bool _ignoreCase;
+
+        public TextContainsLetterFilter(char containingLetter, bool ignoreCase = true)
+        {
+            _containingLetter = containingLetter;
+            _ignoreCase = ignoreCase;
+
+        }
+
+        public bool IsValidText(string text)
+        {
+            if(string.IsNullOrWhiteSpace(text))
+            {
+                return false;
+            }
+
+            return !text.Contains(
+                _containingLetter, 
+                _ignoreCase ? StringComparison.InvariantCultureIgnoreCase: StringComparison.InvariantCulture
+            );
+        }
+    }
+}
