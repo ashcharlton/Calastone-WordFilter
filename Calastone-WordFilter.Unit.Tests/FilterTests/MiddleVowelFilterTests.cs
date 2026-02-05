a7c20f7aca6f0cd629570d51b21412413aaeb727
Ashley Charlton <ashleycharlton@outlook.com>
2026-02-05 11:49:56 +0000

    Added MiddleVowelFilter. Added tests for MiddleVowelFilter

---
+++ Calastone-WordFilter.Unit.Tests/FilterTests/MiddleVowelFilterTests.cs
@@ -0,0 +1,36 @@
+﻿using Calastone_WordFilter_Main.Filters.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calastone_WordFilter.Unit.Tests.FilterTests
+{
+    public class MiddleVowelFilterTests
+    {
+        [Theory]
+        [InlineData("Fact", true, false)]
+        [InlineData("FAct", false, true)]
+        [InlineData("FAct", true, false)]
+        [InlineData("Atta", true, true)]
+        [InlineData("Atta", false, true)]
+        [InlineData("ATTa", false, true)]
+        [InlineData("ATTa", true, true)]
+        [InlineData("", false, false)]
+        [InlineData("", true, false)]
+        [InlineData(null, true, false)]
+        [InlineData(null, false, false)]
+        public void Check_Text_Matches_Vowel_Regex_Should_Return_Correct_Valid_Result(string text, bool ignoreCase, bool isTextValid)
+        {
+            // Arrange
+            var middleVowelFilter = new MiddleVowelFilter(ignoreCase);
+
+            // Act
+            var isValid = middleVowelFilter.IsValidText(text);
+
+            // Assert
+            Assert.Equal(isTextValid, isValid);
+        }
+    }
+}
---
+++ Src/Calastone-WordFilter-Main/Filters/Implementations/MiddleVowelFilter.cs
@@ -0,0 +1,41 @@
+﻿using Calastone_WordFilter_Main.Filters.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Calastone_WordFilter_Main.Filters.Implementations
+{
+    public class MiddleVowelFilter : ITextFilter
+    {
+        private readonly bool _ignoreCase;
+
+        public MiddleVowelFilter(bool ignoreCase)
+        {
+            _ignoreCase = ignoreCase;
+        }
+        
+        public bool IsValidText(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return false;
+            }
+
+            // Odd letter counts will have single middle character
+            // Even letter counts will have 2. 
+            // So getting the substring will require an offset to dynamically tell the substring method the size of the substring.
+            var centerOffset = text.Length % 2 == 0 ? 1 : 0;
+
+            var middleLetters = text.Substring(text.Length / 2 - centerOffset, centerOffset + 1);
+
+            var vowelRegex = new Regex("[aeiou]", _ignoreCase ? RegexOptions.IgnoreCase : RegexOptions.None);
+
+            return !vowelRegex.IsMatch(middleLetters);
+        }
+    }
+}
